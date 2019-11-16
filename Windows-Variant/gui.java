3a967f3758162c8fd1250f31818c3918d4d81305
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-11-16 15:42:06 -0600

    Update gui.java

--- Windows-Variant/gui.java
+++ Windows-Variant/gui.java
@@ -1,12 +1,43 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.*; 
 import java.io.*; 
-import java.net.InetAddress; 
+import java.net.InetAddress;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 class gui{
+	static List<String> logged = new LinkedList<>();
+	private static void runProcess(String command) throws Exception {
+        Process pro = Runtime.getRuntime().exec(command);
+        printLines(command + " stdout:", pro.getInputStream());
+        printLines(command + " stderr:", pro.getErrorStream());
+        pro.waitFor();
+        System.out.println(command + " exitValue() " + pro.exitValue());
+      }
+	
+	private static void printLines(String cmd, InputStream ins) throws Exception {
+        String line = null;
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(ins));
+        while ((line = in.readLine()) != null) {
+            System.out.println(cmd + " " + line);
+            logged.add(cmd + " " + line);
+        }
+      }
+	
     public static void main(String args[])throws Exception {
     	//Creating the Frame
         JFrame frame = new JFrame("CopernicuNetwork");
+        JFrame frame2 = new JFrame("Ping Search");
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         //Creating the MenuBar and adding components
@@ -55,18 +86,42 @@ class gui{
       
         JPanel panel = new JPanel(); // the panel is not visible in output
         JPanel panel1 = new JPanel();
-        JButton IP = new JButton("IP Address");
-        JButton MAC = new JButton("MAC Address");
+        JPanel panel2 = new JPanel();
+        JButton IPS = new JButton("IP Search");
+        JLabel search = new JLabel("Searching...");
+    	
+        
+        IPS.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+          
+            	frame2.setVisible(true);
+            	try {
+            		
+					runProcess("java -cp src src\\Multithread.java");
+					
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+            }
+        });
         JLabel address = new JLabel("Public IP Address is: " + systemipaddress);
         JLabel address1 = new JLabel("System IP Address is: " + localhost.getHostAddress());
         JLabel address2 = new JLabel("Current MAC Address is: " + sb.toString());
-        //panel.add(IP);
+       
         //panel.add(MAC);
         panel1.add(address);
         panel1.add(address1);
         panel1.add(address2);
+        panel.add(IPS);
+        panel2.add(search);
         
         //Adding Components to the frame.
+        frame2.getContentPane().add(BorderLayout.NORTH, panel);
+    	frame2.getContentPane().add(BorderLayout.CENTER, panel2);
         frame.getContentPane().add(BorderLayout.NORTH, panel);
         frame.getContentPane().add(BorderLayout.CENTER, panel1);
         frame.setVisible(true);
