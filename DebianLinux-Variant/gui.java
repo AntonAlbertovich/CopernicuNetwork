422f7018b663c16d2804f1feec73d2de0a98c062
Anton Albertovich <38799794+AntonAlbertovich@users.noreply.github.com>
2019-11-23 18:17:10 -0600

    Add files via upload

--- DebianLinux-Variant/gui.java
+++ DebianLinux-Variant/gui.java
@@ -17,27 +17,8 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
-
 class gui{
         static List<String> logged = new LinkedList<>();
-        private static void runProcess(String command) throws Exception {
-        Process pro = Runtime.getRuntime().exec(command);
-        printLines(command + " stdout:", pro.getInputStream());
-        printLines(command + " stderr:", pro.getErrorStream());
-        pro.waitFor();
-        System.out.println(command + " exitValue() " + pro.exitValue());
-      }
-
-        private static void printLines(String cmd, InputStream ins) throws Exception {
-        String line = null;
-        BufferedReader in = new BufferedReader(
-            new InputStreamReader(ins));
-        while ((line = in.readLine()) != null) {
-            System.out.println(cmd + " " + line);
-            logged.add(cmd + " " + line);
-        }
-      }
-
     public static void main(String args[])throws Exception {
         //Creating the Frame
         JFrame frame = new JFrame("CopernicuNetwork");
@@ -45,11 +26,11 @@ class gui{
         frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame2.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 800);
-        // Returns the instance of InetAddress containing
-        // local host name and address
+        frame.setSize(700, 850);
+        // Returns the instance of InetAddress containing 
+        // local host name and address 
         InetAddress localhost = InetAddress.getLocalHost();
-        // Find public IP address
+        // Find public IP address 
         String systemipaddress = "";
         try
         {
@@ -67,49 +48,73 @@ class gui{
             systemipaddress = "Cannot Execute Properly";
         }
 
-        NetworkInterface networky = NetworkInterface.getByInetAddress(localhost);
 
-        //BUTTON PANEL TIME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //BUTTON AND PANEL CREATION TIME!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         JPanel panel = new JPanel();
-        JPanel panel0 = new JPanel();
-        JPanel panel1 = new JPanel();
         JPanel panel2 = new JPanel();
         JPanel paneltop = new JPanel();
         JLabel range = new JLabel("Search range: ");
+        range.setFont(new Font("Modern No. 20", Font.PLAIN, 27));
         JRadioButton narrow = new JRadioButton("Narrow");
         JRadioButton wide = new JRadioButton("Wide");
         ButtonGroup sweep = new ButtonGroup();
+        wide.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
+        narrow.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
         sweep.add(narrow);
         sweep.add(wide);
-        JLabel scan = new JLabel("Port Scanning: ");
+        JLabel scan = new JLabel("Port scanning: ");
+        scan.setFont(new Font("Modern No. 20", Font.PLAIN, 27));
         JRadioButton on = new JRadioButton("On");
         JRadioButton off = new JRadioButton("Off");
+        on.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
+        off.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
         ButtonGroup port = new ButtonGroup();
         port.add(on);
         port.add(off);
         JLabel air = new JLabel("Air gap: ");
+        air.setFont(new Font("Modern No. 20", Font.PLAIN, 27));
+
         JRadioButton yes = new JRadioButton("On");
         JRadioButton no = new JRadioButton("Off");
+        yes.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
+        no.setFont(new Font("Modern No. 20", Font.PLAIN, 15));
         ButtonGroup gap = new ButtonGroup();
         gap.add(no);
         gap.add(yes);
         JButton GoTime = new JButton("Search");
         JLabel search = new JLabel("Search Complete");
-        search.setFont(new Font("Serif", Font.PLAIN, 36));
+        search.setFont(new Font("Modern No. 20", Font.PLAIN, 36));
         JLabel picLabel = new JLabel(new ImageIcon("tel.png"));
-        final int air_gap_flag = 1;
-        final int port_scan_flag = 1;
-        final int focused_sweep = 1;
+
+        //RADIO BUTTON ACTIONS!!!!!!!!!!!!!
 
 
-
-        //FIPS action
+        //IT'S GO TIME BABY!!!!!!!!!!!
         GoTime.addActionListener(new ActionListener() {
-
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                int air_gap_flag;
+                int port_scan_flag;
+                int focused_sweep;
+              //RADIO BUTTON ACTIONS!!!!!!!!!!!!!
+                   if(on.isSelected()) {port_scan_flag = 1; }
+                   else {port_scan_flag = 0;}
+                   if(yes.isSelected()) {air_gap_flag = 1;}
+                   else {air_gap_flag = 0;}
+                   if(narrow.isSelected()) {focused_sweep = 1;}
+                   else {focused_sweep = 0;}
+
+                   if(off.isSelected()) {port_scan_flag = 0; }
+
+                   if(no.isSelected()) {air_gap_flag = 0;}
+
+                   if(wide.isSelected()) {focused_sweep = 0;}
+
+                   System.out.println(air_gap_flag + "    ");
+                   System.out.print(port_scan_flag + "    ");
+                   System.out.print(focused_sweep + "    ");
                 Multithread a = new Multithread();
                 List<List<String>> logged = new LinkedList<>();
 
@@ -117,24 +122,24 @@ class gui{
                 //air_gap_flag
 
                 // If set to 1 this variable will force a check in with the internet to ensure that the host machine is connected to the internet.
-                // Set to 0 if the host machine is not connected to the internet.
+                // Set to 0 if the host machine is not connected to the internet. 
 
                 //port_scan_flag
 
-                // If set to 1 then every discoverd client on the host's network will have it's porst scanned. This will significantly increase run time.
-                // If set to 0 then ports will not be scanned.
+               // If set to 1 then every discoverd client on the host's network will have it's porst scanned. This will significantly increase run time.
+                // If set to 0 then ports will not be scanned. 
 
                 //focused_sweep
 
-                // If set to 1 then a narrow sweep will be conducted, searching the local IP addresses only directly neighboring the host machine's local address.
+                // If set to 1 then a narrow sweep will be conducted, searching the local IP addresses only directly neighboring the host machine's local address. 
 
-                // Example: If the host machine is at address 192.168.1.25 then a sweep will be conducted from 192.168.1.1 to 192.168.255.255.
-                // Intended for contiguous class C networks.
+                // Example: If the host machine is at address 192.168.1.25 then a sweep will be conducted from 192.168.1.1 to 192.168.255.255. 
+                // Intended for contiguous class C networks. 
 
-                // If set to 0 then a full sweep will be conducted, searching all addresses on a network.
-                // Example: If the host's machine is on 10.170.1.1 then a sweep will be conducted from 10.1.1.1 to 10.255.255.255.
+                // If set to 0 then a full sweep will be conducted, searching all addresses on a network. 
+                // Example: If the host's machine is on 10.170.1.1 then a sweep will be conducted from 10.1.1.1 to 10.255.255.255. 
 
-                // Intended for single class A networks.
+                // Intended for single class A networks. 
 
 
 
@@ -159,9 +164,11 @@ class gui{
         //IP address labels
         JLabel address = new JLabel("Public IP Address is: " + systemipaddress);
         address.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        address.setFont(new Font("Modern No. 20", Font.PLAIN, 17));
         JLabel address1 = new JLabel("System IP Address is: " + localhost.getHostAddress());
         //FORMATTING FORMATTING FORMATTING FORMATTING FORMATTING FORMATTING 
         address1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        address1.setFont(new Font("Modern No. 20", Font.PLAIN, 17));
         panel.add(address);
         panel.add(address1);
         range.setAlignmentX(JLabel.CENTER_ALIGNMENT);
