25afbe8d8ce9e24c5700d2e9aa5a0aee0637e634
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-12-07 22:23:02 -0600

    Update gui.java

--- Windows-Variant/gui.java
+++ Windows-Variant/gui.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 
 class gui{
-	static List<String> logged = new LinkedList<>();
+	
     public static void main(String args[])throws Exception {
     	//Creating the Frame
         JFrame frame = new JFrame("CopernicuNetwork");
